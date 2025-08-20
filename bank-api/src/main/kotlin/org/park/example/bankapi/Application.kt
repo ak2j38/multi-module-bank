81edf9c00a648417136c43db0fae332c4974841e
WooJin Park(Ader) <ak2j38@gmail.com>
2025-08-20 10:07:38 +0900

    feat: 기본 스웨거 설정 추가

---
+++ bank-api/src/main/kotlin/org/park/example/bankapi/Application.kt
@@ -0,0 +1,11 @@
+package org.park.example.bankapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+  runApplication<Application>(*args)
+}
