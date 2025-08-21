84fc6272069d0e291026e1aa2ca56d72aebd07d6
WooJin Park(Ader) <ak2j38@gmail.com>
2025-08-22 00:55:46 +0900

    refactor: 로거 교체

--- bank-event/src/main/kotlin/org/park/example/publisher/EventPublisherImpl.kt
+++ bank-event/src/main/kotlin/org/park/example/publisher/EventPublisherImpl.kt
@@ -1,7 +1,7 @@
 package org.park.example.publisher
 
-import com.sun.org.slf4j.internal.LoggerFactory
 import org.park.example.bankdomain.event.DomainEvent
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
@@ -13,19 +13,19 @@ open class EventPublisherImpl(
   private val logger = LoggerFactory.getLogger(EventPublisherImpl::class.java)
 
   override fun publish(event: DomainEvent) {
-    logger.debug("Publishing event: ${event.eventId} at ${event.occurredOn}")
+    logger.info("Publishing event: ${event.eventId} at ${event.occurredOn}")
     eventPublisher.publishEvent(event)
   }
 
   @Async("taskExecutor")
   override fun publishAsync(event: DomainEvent) {
-    logger.debug("Publishing event: ${event.eventId} at ${event.occurredOn}")
+    logger.info("Publishing event: ${event.eventId} at ${event.occurredOn}")
     eventPublisher.publishEvent(event)
   }
 
   override fun publishAll(events: List<DomainEvent>) {
     events.forEach { event ->
-      logger.debug("Publishing event: ${event.eventId} at ${event.occurredOn}")
+      logger.info("Publishing event: ${event.eventId} at ${event.occurredOn}")
       eventPublisher.publishEvent(event)
     }
   }
@@ -33,7 +33,7 @@ open class EventPublisherImpl(
   @Async("taskExecutor")
   override fun publishAllAsync(events: List<DomainEvent>) {
     events.forEach { event ->
-      logger.debug("Publishing event: ${event.eventId} at ${event.occurredOn}")
+      logger.info("Publishing event: ${event.eventId} at ${event.occurredOn}")
       eventPublisher.publishEvent(event)
     }
   }
